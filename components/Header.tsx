1f0409c7851f09be54db25e27f1e62bfdd7fd206
Joey Jin <Joey_Jin@wistron.com>
2024-12-14 21:01:56 +0800

    :lipstick: style updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import clsx from 'clsx';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
@@ -32,13 +31,11 @@ export default function Header() {
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
-                className={clsx(
-                  'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
-                  {
-                    'text-gray-950 dark:text-gray-200':
-                      activeSection === link.name,
-                  },
-                )}
+                className={
+                  activeSection === link.name
+                    ? 'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300 dark:text-gray-200 text-gray-950'
+                    : 'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-300 dark:text-gray-500'
+                }
                 href={link.hash}
                 onClick={() => {
                   setActiveSection(link.name);
